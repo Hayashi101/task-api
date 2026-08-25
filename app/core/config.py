eddf4aa5066050a749a39eade274667d2eaee056
Hayashi <thanhsonlam.st@gmail.com>
2026-08-25 20:58:00 +0700

    chore: config CORS

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     secret_key: str
     access_token_expire_minutes: int = 30
     refresh_token_expire_minutes: int = 60 * 24 * 7
+    cors_origins: list[str] = []
 
 settings = Settings()
 
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.products import router as product_router
 from app.routers.users import router as user_router
@@ -14,6 +14,13 @@ from app.core.exceptions import (
 )
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(product_router)
